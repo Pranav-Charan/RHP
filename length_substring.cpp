f46242fb41a6cb8b9d5186132916ec52660e67ea
MALLI PRANAV CHARAN <pranavcharanmalli@gmail.com>
2026-06-10 17:19:18 +0530

    Add files via upload

---
+++ length_substring.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1, s2;
+
+    cout << "Enter first string: ";
+    cin >> s1;
+
+    cout << "Enter second string: ";
+    cin >> s2;
+
+    int n = s1.length();
+    int m = s2.length();
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    int maxLen = 0;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+
+            if (s1[i - 1] == s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                maxLen = max(maxLen, dp[i][j]);
+            }
+        }
+    }
+
+    cout << "Length of Longest Common Substring = "
+         << maxLen << endl;
+
+    return 0;
+}
